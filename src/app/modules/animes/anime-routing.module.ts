8ddb5279fb71fb6efe47b065390ffd79ce70ae62
Guilherme Dk <guilhermegsouza520@gmail.com>
2022-03-24 09:25:38 -0300

    Creating score component and routes

--- src/app/modules/animes/anime-routing.module.ts
+++ src/app/modules/animes/anime-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimeListComponent } from './anime-list/anime-list.component';
+import { AnimeScoreComponent } from './anime-score/anime-score.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AnimeListComponent
+  },
+  {
+    path: 'review/:id',
+    component: AnimeScoreComponent
+  },
+  {
+    path: '',
+    redirectTo: 'animes',
+    pathMatch: 'full'
   }
 ];
 
--- src/app/modules/animes/anime-score/anime-score.component.ts
+++ src/app/modules/animes/anime-score/anime-score.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AnimesService } from '../../../core/services/animes.service';
+import { Animes } from '../../../models/animes.model';
 
 @Component({
   selector: 'app-anime-score',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimeScoreComponent implements OnInit {
 
-  constructor() { }
+  anime!: Animes;
+
+  constructor(
+    private route: ActivatedRoute,
+    private service: AnimesService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  getAnime(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if(id !== undefined && id!== null) {
+      this.service.getById(parseInt(id)).subscribe({
+        next: (anime) => (this.anime = anime)
+      })
+    }
+  }
 }
